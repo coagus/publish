5b70ac7adf24812e411429468fcbd5550ba3970f
Christian Agustin <networker@christianagustin.com>
2022-03-18 07:28:27 -0600

    finish first part

--- index.js
+++ index.js
@@ -1,14 +1,161 @@
+
 const xlsx = require("xlsx");
+const oracledb = require('oracledb');
+const axios = require('axios')
 
-let file = process.argv.slice(2)[0];
+function post(project,task) {
+  console.log(task+' '+project)
+  axios
+  .post('http://100.126.0.13:7004/ecm/ecm/CatalogManagement/v2/project/'+project+'/'+task+'', {
+    headers: {
+        'OnBehalfOf': 'upadmin'
+      }
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
 
-console.log(file);
+async function run() {
+  let connection
+  
+  try {
+    connection = await oracledb.getConnection({ user: "ECM01", password: "ECM01", connectionString: "100.126.1.96:1521/D_EOC01" })
+    console.log("Successfully connected to Oracle Database")
 
-const workbook = xlsx.readFile(file);
-const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    let file = process.argv.slice(2)[0]
 
-for (let z in worksheet) {
-  if(z.toString()[0] === 'A') {
-    console.log(worksheet[z].v)
+    console.log('Read file ',file)
+    const book = xlsx.readFile(file)
+
+    console.log('Read sheet Projects')
+    const sheet = book.Sheets['Projects']
+
+    if (sheet != null) {
+      console.log('Read project list in col A')
+      for (let cell in sheet) {    
+        if(cell.toString()[0] === 'A' && cell.toString()[1] !== '1') {
+          var projectName = sheet[cell].v;
+          console.log('project name',projectName)
+          result = await connection.execute(
+            `select * from ECM01.cwpc_project where projectcode = :project`,
+            [projectName],
+            { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT })
+          
+          const rs = result.resultSet
+          let status = 'DEF'
+          let project = await rs.getRows(0)
+
+          if (project.length > 0) {
+            console.log('status: ', project[0].STATUS)
+            status = project[0].STATUS
+          } else {
+            console.log('Project not exists')
+          }
+          await rs.close();
+
+          if (status == 'ACT') {
+            console.log('Set Status to DEF')
+            await connection.execute(
+              "begin setCatalogProjectStatus(:project,'DEF'); commit; end;", 
+              [projectName]
+            );
+          }
+
+          post(projectName,"validate")
+          post(projectName,"publish")
+        }    
+      }
+    }
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
   }
 }
+
+run()
+
+
+//var orig = '   foo  ';
+//console.log('hola ',orig.trim()); // 'foo'
+
+
+
+
+/*
+const oracledb = require('oracledb');
+
+async function run() {
+
+  let connection;
+
+  try {
+
+    connection = await oracledb.getConnection({ user: "ECM01", password: "ECM01", connectionString: "100.126.1.96:1521/D_EOC01" });
+
+    console.log("Successfully connected to Oracle Database");
+
+    // Now query the rows back
+
+    result = await connection.execute(
+      `select * from ECM01.cwpc_project where projectcode = 'TestRuben1'`,
+      [],
+      { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT });
+
+    const rs = result.resultSet;
+    let row;
+
+    while ((row = await rs.getRow())) {
+        console.log(row.STATUS, " is actual Status");
+    }
+
+    await rs.close();
+
+    await connection.execute(
+        "begin setCatalogProjectStatus('TestRuben1','ACT'); commit; end;"
+      );
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
+
+run();
+
+
+const axios = require('axios')
+
+axios
+  .post('http://100.126.0.13:7004/ecm/ecm/CatalogManagement/v2/project/AmxCoMovPosOptDesc007_B22/publish', {
+    headers: {
+        'OnBehalfOf': 'upadmin'
+      }
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+*/
