9d82ba9874af07421001a3dbb9af27979964206c
Christian Agustin <networker@christianagustin.com>
2022-03-28 07:03:18 -0600

    se generan los xml de bscs

--- src/publish.js
+++ src/publish.js
@@ -2,6 +2,10 @@ const oracledb = require('oracledb')
 const axios = require('axios')
 const xlsx = require("xlsx")
 const config = require('config')
+const fs = require('fs')
+const util = require('util')
+
+const writeFile = util.promisify(fs.writeFile)
 
 const isValidProjectDB = async (projectName) => {
     console.log('Valid',projectName,'in DB')
@@ -75,14 +79,89 @@ const postProject = async (projectName, task) => {
     return result
 }
 
+const getTraces = async (projectList, pathBSCS) => {
+    if (!fs.existsSync(pathBSCS)){
+        fs.mkdirSync(pathBSCS)
+    }
+
+    const dbConfig = config.get('dbConfig')
+    try {
+        connection = await oracledb.getConnection(dbConfig)
+
+        let qry = `SELECT pr.projectcode, prcmm.catalogobjectcode
+                     FROM ecm01.cwpc_project pr
+                     JOIN ecm01.cwpc_projectcommand prcmm ON pr.projectid = prcmm.projectid
+                    WHERE pr.projectcode IN (`;
+
+        for (let i=0; i < projectList.length; i++) {
+            qry += ((i>0) ? ", :v" : ":v") + i
+        }
+
+        qry += `) GROUP BY pr.projectcode, prcmm.catalogobjectcode`
+
+        let result = await connection.execute(qry, projectList, {resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT})          
+        let rs = result.resultSet
+        let row, dat, item
+        let proys = []
+        while ((row = await rs.getRow())) {
+            dat = `{"project": "${row.PROJECTCODE}", "po": "${row.CATALOGOBJECTCODE}"}`
+            item = JSON.parse(dat)
+            proys.push(item)            
+        }          
+        await rs.close()  
+
+        result = await connection.execute(
+            `SELECT creation_time, send_data 
+               FROM ecm01.CWMESSAGELOG
+              WHERE operation = 'billingAdapter.BSCSAdapter.productofferingwrite:ProductOfferingWriteService/productOfferingWrite'
+                AND user_id='upadmin'
+                AND creation_time > sysdate-1`,{},
+            {resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT, fetchInfo: {"SEND_DATA": {type: oracledb.BUFFER}}})
+          
+        rs = result.resultSet
+        let trace
+
+        while ((row = await rs.getRow())) {
+            trace = row.SEND_DATA.toString()
+            if (!trace.includes('billingAdapter.BSCSAdapter.productofferingwrite:sessionChangeRequest')) {
+                for (const proy of proys) {
+                    if (trace.includes(proy.po)) {
+                        if (!fs.existsSync(`${pathBSCS}/${proy.project}.xml`)) {
+                            console.log(`Save trace: ${proy.project}.xml`)
+                            writeFile(`${pathBSCS}/${proy.project}.xml`,row.SEND_DATA.toString())
+                        }                   
+                        break
+                    }
+                }                
+            }
+        }          
+        await rs.close()
+    } catch (err) {
+        console.log('Database error:')
+        console.error(err)
+    } finally {
+        if (connection) {
+            try {
+                await connection.close()
+            } catch (err) {
+                console.log('Error while to close DB:')
+                console.error(err)
+            }
+        }
+    }
+}
+
 async function run() {
     let file = process.argv.slice(2)[0]
+    let pathBSCS = file.replace('publish.xlsx','BSCS')
 
     console.log('Read file',file)
     const book = xlsx.readFile(file)
 
     console.log('Read sheet Projects')
     const sheet = book.Sheets['Projects']
+
+    let projectList = []
 
     if (sheet != null) {
         console.log('Read project list in column A')
@@ -93,13 +172,16 @@ async function run() {
                 if (await isValidProjectDB(projectName)
                     && await postProject(projectName,'validate')
                     && await postProject(projectName,'publish')) {
-                    console.log('***** ',projectName, 'Published! *****')  
+                    console.log('***** ',projectName, 'Published! *****') 
+                    projectList.push(projectName)
                 }
             }
         }
     } else {
         console.log('Not exists sheet Projects in this file')
-    }    
+    }
+
+    getTraces(projectList, pathBSCS)
 }
 
 run()
